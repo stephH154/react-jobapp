226af37b5999bc29c31f0008c8757b1f8f76e7a6
Stephen Hsieh GitHub project <90292524+ang154@users.noreply.github.com>
2024-06-13 18:13:06 -0400

    add delete

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,42 @@ import {
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
-import AddJobs from "./pages/AddJobs";
+import AddJobs from "./pages/AddJobPage";
 import NotFound from "./pages/NotFound";
+import JobPage, { jobLoader } from "./pages/JobPage";
 
 const App = () => {
+  // Add new job
+  const addJob = async (newJob) => {
+    const res = await fetch("/api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  // Delete exist job
+  const deleteJob = async (i) => {
+    const res = await fetch(`/api/jobs/${i}`, {
+      method: "DELETE",
+    });
+    console.log("DELETE" + i);
+  };
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
-        <Route path="/add-job" element={<AddJobs />} />
+        <Route
+          path="/jobs/:id"
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
+        <Route path="/add-job" element={<AddJobs addJobSubmit={addJob} />} />
         <Route path="/*" element={<NotFound />} />
       </Route>
     )
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbaro";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const MainLayout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <ToastContainer />
     </>
   );
 };
